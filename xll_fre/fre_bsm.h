5805ba7349ea181c50c991e29a0a88edbec57451
Keith A. Lewis <keithalewis@live.com>
2023-10-15 13:29:48 -0400

    use math.h instead of cmath bsm_to_black

--- xll_fre/fre_bsm.h
+++ xll_fre/fre_bsm.h
@@ -3,34 +3,41 @@
 // Black price at expiration is F = f exp(s Z - s^2/2), Z standard normal.
 // Note S_t = F where f = S0 exp(rt) and s = σ√t.
 #pragma once
+#include <tuple>
 #include "fre_black.h"
 
 namespace fre::bsm {
 
 #pragma warning(disable: 4100)
+
+	// Convert from Black-Scholes/Merton to Black parameters.
+	inline std::tuple<double, double, double> bsm_to_black(double r, double S0, double σ, double t)
+	{
+		double R = exp(r * t);
+
+		return { R, R * S0, σ * sqrt(t) }; // R, f, s
+	}
+
 	namespace put {
-		// Value is E[(k - S_t)^+]/R, where R = exp(r t).
+		// Put value is E[(k - S_t)^+]/R, where R = exp(r t).
 		//                  ------------market-----------
 		//                  --bond--|-------stock--------  ------option------
 		inline double value(double r, double S0, double σ, double k, double t)
 		{
-			double R = std::exp(r*t); 
-			double f = R*S0; 
-			double s = σ*std::sqrt(t);
+			auto [R, f, s] = bsm_to_black(r, S0, σ, t);
 
-			return fre::black::put::value(f, s, k)/R;  
+			return black::put::value(f, s, k)/R;  
 		}
 		// Delta is (d/dS0)E[(k - S_t)^+]/R = (d/df)E[(k - F)^+]/R df/dS0.
 		// df/dS0 = R
 		inline double delta(double r, double S0, double σ, double k, double t)
 		{
-			double R = std::exp(r * t);
-			double f = R * S0;
-			double s = σ * std::sqrt(t);
+			auto [R, f, s] = bsm_to_black(r, S0, σ, t);
 
-			return fre::black::put::delta(f, s, k);
+			return black::put::delta(f, s, k);
 		}
 	}
+
 } // namespace fre::bsm
 
 
---
+++ xll_fre/fre_test.h
@@ -0,0 +1,36 @@
+// fre_test.h - test routines
+#pragma once
+#include <tuple>
+
+namespace fre::test {
+
+	// f(x + h) - f(x - h) = f(x) + f'(x) h + (1/2) f''(x) h^2 + (1/3!) f'''(x) h^3 + O(h^4)
+	//                     - f(x) + f'(x) h - (1/2) f''(x) h^2 + (1/3!) f'''(x) h^3 + O(h^4)
+	//                     = 2 f'(x) h + (2/3!) f'''(x) h^3 + O(h^4)
+	// (f(x + h) - f(x - h))/2h = f'(x) + (1/6) f'''(x) h^2 + O(h^3)
+	template<class F, class X = double>
+	inline X symmetric_difference_quotient(const F& f, X x, X h)
+	{
+		return (f(x + h) - f(x - h)) / (2 * h);
+	}
+
+	// Return mean and variance of N samples.
+	// https://imstat.org/2015/11/17/the-kids-are-alright-divide-by-n-when-estimating-variance/
+	template<class F, class X = double>
+	inline std::tuple<X, X> monte_carlo_mean_variance(const F& f, size_t N)
+	{
+		X m = 0, v2 = 0;
+
+		// m_n = (x_0 + ... + x_n)/n
+		// n m_n - (n - 1) m_{n-1} = x_n
+		// m_n = m_{n-1} + (x_n - m_{n-1})/n
+		for (size_t n = 1; n <= N; ++n) {
+			X x = f();
+			m += (x - m) / n;
+			v2 += (x * x - v2) / n;
+		}
+
+		return { m, v2 - m * m };
+	}
+
+} // fre::test
