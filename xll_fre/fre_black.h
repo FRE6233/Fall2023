97427822f13f02217a98fb4c0a37442855536ac8
Keith A. Lewis <keithalewis@live.com>
2023-11-01 10:24:20 -0400

    black vega

--- xll_fre/fre_black.h
+++ xll_fre/fre_black.h
@@ -37,7 +37,7 @@ namespace fre::black {
 		{
 			double m = moneyness(f, k, s);
 
-			return -normal::cdf(m, s) * (m - s); //!!! fix this
+			return f*normal::pdf(m, s); //!!! fix this
 		}
 #ifdef _DEBUG
 		inline int vega_test()
