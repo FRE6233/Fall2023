178af55a99ed16db72f41eed4aa574449c10940d
Keith A. Lewis <keithalewis@live.com>
2023-11-03 08:24:49 -0400

    hw8

---
+++ xll_fre/fre_ho_lee.h
@@ -0,0 +1,40 @@
+﻿// fre_ho_lee.h - Ho-Lee interest rate model for constant futures and volatility.
+#pragma once
+#include <cmath>
+#include <random>
+
+namespace fre::ho_lee {
+
+	// E[exp(N)] = exp(E[N] + Var(N)/2)
+	inline double Eexp(const std::normal_distribution<double>& N)
+	{
+		return std::exp(N.mean() + N.sigma() * N.sigma() / 2);
+	}
+
+	// D(t) = E[D_t] = ???
+	inline double ED(double φ, double σ, double t)
+	{
+		return φ * σ * t; // !!!
+	}
+
+	// φ(t) - f(t) = ???
+	inline double convexity(double σ, double t)
+	{
+		return σ * t; // !!!
+	}
+
+	// log D_t(u) = ???
+	// Return mean and standard deviation of log D_t(u).
+	inline std::normal_distribution<double> logD(double φ, double σ, double t, double u)
+	{
+		return std::normal_distribution(φ, σ * t * u); // !!!
+	}
+
+	// φ_t(u) - f_t(u) = ???
+	// Return mean and standard deviation of φ_t(u) - f_t(u)
+	inline std::normal_distribution<double> convexity(double σ, double t, double u)
+	{
+		return std::normal_distribution(t*u, σ); // !!!
+	}
+
+} // namespace fre::ho_lee
---
+++ xll_fre/xll_ho_lee.cpp
@@ -0,0 +1,6 @@
+// xll_ho_lee.cpp - Ho-Lee interest rate add-ins
+#include "fre_ho_lee.h"
+#include "xll_fre.h"
+
+using namespace fre::ho_lee;
+using namespace xll;
