9c8434b20098ed0625925400d0fec303887658d7
Keith A. Lewis <keithalewis@live.com>
2023-11-22 08:29:49 -0500

    ho_lee::convexity

--- xll_fre/xll_ho_lee.cpp
+++ xll_fre/xll_ho_lee.cpp
@@ -10,12 +10,28 @@ using namespace xll;
 // HO_LEE.Convexity
 // HO_LEE.LogD
 
+AddIn xai_ho_lee_ED(
+	Function(XLL_DOUBLE, "xll_ho_lee_ED", "HO_LEE.ED")
+	.Arguments({
+		Arg(XLL_DOUBLE, "φ", "is the futures."),
+		Arg(XLL_DOUBLE, "σ", "is the volatility."),
+		Arg(XLL_DOUBLE, "t", "is the forward time in years"),
+		})
+		.FunctionHelp("Return the expected discount.")
+	.Category(CATEGORY)
+);
+double WINAPI xll_ho_lee_ED(double φ, double σ, double t)
+{
+#pragma XLLEXPORT
+	return ho_lee::ED(φ, σ, t);
+}
+
 // This illustrates how to return an array of doubles to Excel.
 AddIn xai_ho_lee_LogD(
 	Function(XLL_FPX, "xll_ho_lee_LogD", "HO_LEE.LogD")
 	.Arguments({
 		Arg(XLL_DOUBLE, "φ", "is the futures."),
-		Arg(XLL_DOUBLE, "σ", "is the volatiltiy."),
+		Arg(XLL_DOUBLE, "σ", "is the volatility."),
 		Arg(XLL_DOUBLE, "t", "is the forward time in years"),
 		Arg(XLL_DOUBLE, "u", "is the time to futures expiration in years"),
 		})
@@ -42,11 +58,26 @@ _FPX* WINAPI xll_ho_lee_LogD(double φ, double σ, double t, double u)
 	return stdev.get();
 }
 
+AddIn xai_ho_lee_convexity(
+	Function(XLL_DOUBLE, "xll_ho_lee_convexity", "HO_LEE.Convexity")
+	.Arguments({
+		Arg(XLL_DOUBLE, "σ", "is the volatility."),
+		Arg(XLL_DOUBLE, "t", "is the forward time in years"),
+		})
+		.FunctionHelp("Return the convexity at time t.")
+	.Category(CATEGORY)
+);
+double WINAPI xll_ho_lee_convexity(double σ, double t)
+{
+#pragma XLLEXPORT
+	return ho_lee::convexity(σ, t);
+}
+
 // This illustrates how to return an array of doubles to Excel.
 AddIn xai_ho_lee_convexity2(
 	Function(XLL_FPX, "xll_ho_lee_convexity2", "HO_LEE.Convexity2")
 	.Arguments({
-		Arg(XLL_DOUBLE, "σ", "is the volatiltiy."),
+		Arg(XLL_DOUBLE, "σ", "is the volatility."),
 		Arg(XLL_DOUBLE, "t", "is the forward time in years"),
 		Arg(XLL_DOUBLE, "u", "is the time to futures expiration in years"),
 		})
