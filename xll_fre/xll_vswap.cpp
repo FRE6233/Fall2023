8a784566dd381a3f545f45ab265f557861673a49
Keith A. Lewis <kal@kalx.net>
2023-12-21 13:45:29 -0500

    final

---
+++ xll_fre/xll_vswap.cpp
@@ -0,0 +1,11 @@
+// vswap.cpp
+#include "fre_vswap.h"
+#include "xll_fre.h"
+
+using namespace xll;
+
+//!!! implement vswap::variance
+AddIn xai_vswap(
+	Function(XLL_DOUBLE, "xll_vswap", "XLL.VSWAP")
+	//...
+);
